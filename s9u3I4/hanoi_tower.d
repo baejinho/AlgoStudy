4c26fbf8405559170e6253e6a760d4ddf075277c
Jeon Yoeun <sev@peachbyte.co.kr>
2017-06-20 12:30:58 +0900

    하노이 타워

--- s9u3I4/hanoi_tower.d
+++ s9u3I4/hanoi_tower.d
@@ -2,7 +2,20 @@ import std.stdio;
 
 enum MOD = 1_000_000_007;
 
-int hanoi_tower(int[] counts) {
+int[] countsFromSizes(int[] sizes) {
+  int[] result = [];
+  int prev;
+  foreach (size; sizes) {
+    if (result.length > 0 && size == prev)
+      result[result.length - 1]++;
+    else
+      result ~= 1;
+    prev = size;
+  }
+  return result;
+}
+
+int hanoiTower(int[] counts) {
   int result = 0;
   foreach (count; counts) {
     result = (2 * result) % MOD;
@@ -12,12 +25,30 @@ int hanoi_tower(int[] counts) {
 }
 
 void main() {
-  writeln(hanoi_tower([]));
-  writeln(hanoi_tower([1]));
-  writeln(hanoi_tower([2]));
-  writeln(hanoi_tower([1, 1]));
-  writeln(hanoi_tower([3]));
-  writeln(hanoi_tower([2, 1]));
-  writeln(hanoi_tower([1, 2]));
-  writeln(hanoi_tower([1, 1, 1]));
+  writeln(countsFromSizes([]));
+  writeln(countsFromSizes([1]));
+  writeln(countsFromSizes([1, 1]));
+  writeln(countsFromSizes([1, 2]));
+  writeln(countsFromSizes([1, 1, 1]));
+  writeln(countsFromSizes([1, 1, 2]));
+  writeln(countsFromSizes([1, 2, 2]));
+  writeln(countsFromSizes([1, 2, 3]));
+
+  writeln(hanoiTower([]));
+  writeln(hanoiTower([1]));
+  writeln(hanoiTower([2]));
+  writeln(hanoiTower([1, 1]));
+  writeln(hanoiTower([3]));
+  writeln(hanoiTower([2, 1]));
+  writeln(hanoiTower([1, 2]));
+  writeln(hanoiTower([1, 1, 1]));
+
+  writeln(hanoiTower(countsFromSizes([])));
+  writeln(hanoiTower(countsFromSizes([1])));
+  writeln(hanoiTower(countsFromSizes([1, 1])));
+  writeln(hanoiTower(countsFromSizes([1, 2])));
+  writeln(hanoiTower(countsFromSizes([1, 1, 1])));
+  writeln(hanoiTower(countsFromSizes([1, 1, 2])));
+  writeln(hanoiTower(countsFromSizes([1, 2, 2])));
+  writeln(hanoiTower(countsFromSizes([1, 2, 3])));
 }
